17ed449e4dae3fb433cbdf3943686399fe387563
figlerr <figler.renata@tmx.hu>
2021-08-19 11:04:59 +0200

    forras

---
+++ src/main/java/hu/tmx/spring/spring/UdemySpringAlapokApplication.java
@@ -0,0 +1,13 @@
+package hu.tmx.spring.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemySpringAlapokApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UdemySpringAlapokApplication.class, args);
+    }
+
+}
