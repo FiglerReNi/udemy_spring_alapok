a753631217342018ba3f137a681de7c10ebc4ab0
figlerr <figler.renata@tmx.hu>
2021-08-23 11:58:17 +0200

    forras

--- src/main/java/hu/tmx/spring/spring/UdemySpringAlapokApplication.java
+++ src/main/java/hu/tmx/spring/spring/UdemySpringAlapokApplication.java
@@ -1,32 +1,25 @@
 package hu.tmx.spring.spring;
 
 import hu.tmx.spring.spring.service.*;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@Configuration
+@ComponentScan
 public class UdemySpringAlapokApplication {
 
     public static void main(String[] args) {
-        ApplicationContext applicationContext =  SpringApplication.run(UdemySpringAlapokApplication.class, args);
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(UdemySpringAlapokApplication.class);
 
-        BinarySearchSpring binarySearchSpring = applicationContext.getBean(BinarySearchSpring.class);
-        int result1 = binarySearchSpring.doBinarySearch(new int[] {124,6}, 3);
-        System.out.println(result1);
         BinarySearchQualifier binarySearchQualifier = applicationContext.getBean(BinarySearchQualifier.class);
-        int result2 = binarySearchQualifier.doBinarySearch(new int[] {124,6}, 3);
-        System.out.println(result2);
+        int result = binarySearchQualifier.doBinarySearch(new int[] {124,6}, 3);
+        System.out.println(result);
 
-        BinarySearchSpring binarySearch1 = applicationContext.getBean(BinarySearchSpring.class);
-        System.out.println(binarySearch1);
-        BinarySearchSpring binarySearch2 = applicationContext.getBean(BinarySearchSpring.class);
-        System.out.println(binarySearch2);
 
-        BinarySearchScope binarySearchScope1 = applicationContext.getBean(BinarySearchScope.class);
-        System.out.println(binarySearchScope1);
-        BinarySearchScope binarySearchScope2 = applicationContext.getBean(BinarySearchScope.class);
-        System.out.println(binarySearchScope2);
+
+
     }
 
 }
